ba14c9a22f186f479615f016db41268d8662edf9
Abhishekiet <62215477+Abhishekiet@users.noreply.github.com>
2023-09-22 12:56:02 +0530

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,15 +1,30 @@
+'use client'
+
 import Add from "./components/Add"
 import Container from "./components/Container"
+import { useSession } from "next-auth/react"
+import { useRouter } from "next/navigation"
 
 const App=()=>{
-  return(
-    <div className="min-h-[76vh]">
 
-      <Add />
-      <h1 className="text-3xl font-bold text-center underline underline-offset-4 mt-6"> Your List Items </h1>
-      <Container />
-    </div>
-  )
+  const session = useSession()
+  const router = useRouter()
+
+  if( session.status === 'unauthenticated' ){
+    router.push("/signin")
+  }
+
+  else{
+    return(
+      <div className="min-h-[76vh]">
+  
+        <Add />
+        <h1 className="text-3xl font-bold text-center underline underline-offset-4 mt-6"> Your List Items </h1>
+        <Container />
+      </div>
+    )
+  }
+  
 }
 
 export default App
